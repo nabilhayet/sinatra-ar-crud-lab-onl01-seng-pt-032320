11eb491415aba07505f853004d784f37da8a60df
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-24 22:03:07 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,22 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(title:params[:title], content:params[:content])
+    redirect to 'articles/#{@article.id}'
+  end
+
+  get 'articles/:id' do
+    @article = params[:id]
+    erb :show
   end
 end
